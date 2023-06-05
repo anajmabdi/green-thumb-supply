373b30802181250f2f33def171bc655a7c05e396
Najma Abdi <anajmabdi@gmail.com>
2023-06-04 18:12:38 -0700

    clean up scaffolding, render header & class

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import PlantControl from "./PlantControl"
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <PlantControl/>
+    </>
+  );
+}
+
+export default App;
