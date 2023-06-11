3f5eb76f5bc0ec9a4eea53ea63058de71b7ed12f
Najma Abdi <anajmabdi@gmail.com>
2023-06-11 12:30:12 -0700

    updated display of plant component

--- src/components/Plant.js
+++ src/components/Plant.js
@@ -1,27 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Plant({name, price}) {
+function Plant(props){
     return (
-        <>
-        <h3>{name} </h3> 
-        <p>{scientificName} </p>
-        <p>{price} </p>
-        <p>{plantingSeasons} </p>
-        <p>{pests} </p>
-        <p>{needs} </p>
-        </>
-    )
-
-}
-
-
-Plant.propTypes = {
+      <>
+      <div onClick = {() => props.whenPlantClicked(props.id)}>
+      <h3>{props.name} </h3> 
+      <p>${props.price} </p>
+      </div>
+      </> 
+    );
+  }
+  
+  Plant.propTypes = {
     name: PropTypes.string,
     scientificName: PropTypes.string,
     pests: PropTypes.string,
     price: PropTypes.number,
     plantingSeasons: PropTypes.string,
-    needs: PropTypes.string
-}
+    needs: PropTypes.string,
+    id: PropTypes.string,
+    whenPlantClicked: PropTypes.func
+  };
+  
 export default Plant;
