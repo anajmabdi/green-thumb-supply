10f7370c4a4161d816d45358152d259a60a23b7e
Najma Abdi <anajmabdi@gmail.com>
2023-06-11 12:40:47 -0700

    add more props

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -8,7 +8,11 @@ function NewPlantForm(props) {
         event.preventDefault();
         props.onNewPlantCreation({
             name: event.target.name.value,
+            scientificName: event.target.scientificName.value,
             price: event.target.price.value,
+            pests: event.target.pests.value,
+            plantingSeasons: event.target.plantingSeasons.value,
+            needs: event.target.needs.value,
             id: v4()
         });
     }
@@ -20,16 +24,37 @@ function NewPlantForm(props) {
                     type='text'
                     name='name'
                     placeholder='Name' />
+
+                <input
+                    type='text'
+                    name='scientificName'
+                    placeholder='Scientific Name' />
+
                 <input
                     type='text'
                     name='price'
                     placeholder='Price' />
+
+                <input
+                    type='text'
+                    name='pests'
+                    placeholder='Pests' />
+
+<input
+                    type='text'
+                    name='plantingSeasons'
+                    placeholder='Planting Seasons' />
+
+<input
+                    type='text'
+                    name='needs'
+                    placeholder='Needs' />
+
                 <button type='submit'>Submit</button>
             </form>
         </>
     );
 }
-
 NewPlantForm.propTypes = {
     onNewPlantCreation: PropTypes.func
 };
